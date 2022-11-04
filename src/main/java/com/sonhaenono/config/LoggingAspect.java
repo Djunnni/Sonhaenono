782cf6b2536c40a892da39c926426182596850d5
Dongjoon Lee <djunnni@gmail.com>
2022-11-04 22:54:16 +0900

    fix: spring-aop execution option update

--- src/main/java/com/sonhaenono/config/LoggingAspect.java
+++ src/main/java/com/sonhaenono/config/LoggingAspect.java
@@ -9,24 +9,26 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LoggingAspect {
 	private Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 	
-	@Before(value = "execution(* com.sonhaenono.member.*.*(..))")
+	@Before(value = "execution(* com.sonhaenono.member..*.*(..))")
 	public void loggin(JoinPoint joinPoint) {
 		logger.debug("before call method : {}", joinPoint.getSignature());
 		logger.debug("메서드 선언부: {} 전달 파라미터 : {}", joinPoint.getSignature(), Arrays.toString(joinPoint.getArgs()));
 	}
 
-	@AfterReturning(value = "execution(* com.sonhaenono.member.*.*(..))", returning = "obj")
+	@AfterReturning(value = "execution(* com.sonhaenono.member..*.*(..))", returning = "obj")
 	public void afterReturningMethod(JoinPoint joinPoint, Object obj) {
 		logger.debug("afterReturning call method : {} ", joinPoint.getSignature());
 		logger.debug("return value : {}", obj);
 	}
 	
-	@AfterThrowing(value = "execution(* com.sonhaenono.member.*.*(..))", throwing = "exception")
+	@AfterThrowing(value = "execution(* com.sonhaenono.member..*.*(..))", throwing = "exception")
 	public void afterThrowingMethod(JoinPoint joinPoint, Exception exception) {
 		logger.debug("afterThrowing call method : {}", joinPoint.getSignature());
 		logger.debug("exception : {}", exception);
